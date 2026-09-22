8e1a269f10f798cf67188970697294ea30c37724
affan soomro <affansoomro1122@gmail.com>
2026-09-23 01:52:54 +0500

    Fixing The Search Recipe Problem
    I That code i fixed the recipe search bar. 
    in old code if someone search the recipe like chicken, so it got the recipes about chicken but after that a person searched a wrong recipe name so the old chicken recipe are not gone and wrong name alert is not come to the screen... 
    Now in new code this problem is fixed

--- Recipes FInder/assets/js/script.js
+++ Recipes FInder/assets/js/script.js
@@ -1,9 +1,10 @@
 const inputData = document.querySelector("#inputRecipe");
 let submit = document.querySelector(".top-row button");
 let recipeFlex = document.querySelector(".flex-boxes");
+let recipeBOX = document.querySelector(".flex-boxes .recipe-box");
 
 async function getRecipe() {
-    const ErrorAlert = document.querySelector("#ErrorAlert");
+  const ErrorAlert = document.querySelector("#ErrorAlert");
 
   if (inputData.value === "") {
     alert("Please Enter Some Recipe Name");
@@ -18,28 +19,34 @@ async function getRecipe() {
   let mealsRecipe = recipe.meals;
   console.log(mealsRecipe);
 
-//   Error alert for wrong recipe name
-  if(!recipe.meals){
-    ErrorAlert.style.display = "block"
-  }
+  // Error alert for wrong recipe name
+  if (!recipe.meals) {
+    ErrorAlert.style.display = "block";
+    recipeFlex.style.display = "none";
+    inputData.value = "";
+  }else{
+    ErrorAlert.style.display = "none";
+    recipeFlex.style.display = "flex";
 
 
   let interator = "";
   mealsRecipe.forEach((e) => {
     interator += `
-                    <a href="recipes.html?id=${e.idMeal}" class="recipe-box">
-                        <img src="${e.strMealThumb}" alt="" class="img-fluid">
-                        <h1 id="">${e.strMeal}</h1>
-                    </a>`;
+                      <a href="recipes.html?id=${e.idMeal}" class="recipe-box">
+                          <img src="${e.strMealThumb}" alt="" class="img-fluid">
+                          <h1 id="">${e.strMeal}</h1>
+                      </a>`;
   });
   recipeFlex.innerHTML = interator;
+  // inputData.value = "";  
+  }
 }
 
-  submit.addEventListener("click", () => {
+submit.addEventListener("click", () => {
+  getRecipe();
+});
+inputData.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
     getRecipe();
-  });
-  inputData.addEventListener("keydown", function (event){
-    if(event.key === "Enter"){
-       getRecipe();
-    }
-  })
+  }
+});
